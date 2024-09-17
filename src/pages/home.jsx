b130f52ef40ce7b764c38cf3be33fc27758e8d4f
myprojects <shubhamgar4488@gmail.com>
2024-09-18 04:34:34 +0530

    first commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function home() {
+  return (
+    <div className='homepage-container'>
+        <div className="input-container">
+
+      <input placeholder='enter your email' type="email" name="" id="" />
+      <input type="text" name="" id="" placeholder='enter your room code' />
+      <button> enter room</button>
+        </div>
+    </div>
+  )
+}
+
+export default home
---
+++ src/providers/Socket.jsx
@@ -0,0 +1,14 @@
+import React,{useMemo} from 'react'
+import {io} from 'socket.io-client'
+
+const Socketcontext = React.createContext(null)
+
+function Socket() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Socket
